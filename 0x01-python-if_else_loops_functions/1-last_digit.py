0b44c4affdbb8c72611ace1d4ec609084332f59e
leinefran <penny.leine@gmail.com>
2018-09-12 23:47:19 +0000

    Deleted: if statement using abs() functionand Added: 2 if statements: one if number is negative and another for a positive number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = abs(number) % 10
+if number < 0:
+    ld = number % -10
+else:
+    ld = number % 10
+
 if ld > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".
           format(number, ld))
