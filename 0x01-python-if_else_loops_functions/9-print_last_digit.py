5ac3d209d2a4d71766a422d1d2b66b7faa063871
leinefran <penny.leine@gmail.com>
2018-09-12 23:55:02 +0000

    Deleted: if and else statements. Added: a variable ld and assigned the last digit to it. Used function abs() to handle negative numbers

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        ld = number % -10
-        print(ld)
-        return(ld)
-    else:
-        ld = number % 10
-        print(ld)
-        return(ld)
+    ld = abs(number) % 10
+    print(ld, end="")
+    return ld
