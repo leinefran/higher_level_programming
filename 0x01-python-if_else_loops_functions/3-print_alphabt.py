f6b442fcb8a0d3ef2315bd3aae809f7d28ce1615
leinefran <penny.leine@gmail.com>
2018-09-12 15:46:53 +0000

    Fixed: identation

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z')+1):
     if alpha_letters == ord('e') or alpha_letters == ord('q'):
-       continue
+        continue
     print("{:c}".format(alpha_letters), end="")
