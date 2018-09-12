c56b6303b7eadc33091f8ddfb0f347c1f90eb3e3
leinefran <penny.leine@gmail.com>
2018-09-12 23:33:19 +0000

    Changed: if statement synthax

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if c in range(ord('a'), ord('z')+1):
-        return (True)
-    return (False)
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    return False
