466119aa85661839bfdb8e6bd1d46681cb5947e9
leinefran <penny.leine@gmail.com>
2018-09-13 23:18:53 +0000

    Fixed: a typo in the first print call

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{:d} arguments:". format(0))
-    elif len(sys.argv) == 2:
-        print("{:d} argument:". format(len(sys.argv)-1))
-        print("{:d}: {:s}".format(1, sys.argv[1]))
+    argv = sys.argv
+    if len(argv) == 1:
+        print("{:d} arguments.". format(0))
+    elif len(argv) == 2:
+        print("{:d} argument:". format(len(argv)-1))
+        print("{:d}: {:s}".format(1, argv[1]))
     else:
-        print("{:d} arguments:". format(len(sys.argv)-1))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d} arguments:". format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
