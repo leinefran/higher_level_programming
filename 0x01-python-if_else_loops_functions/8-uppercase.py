5894486602926819d7801f2d34c5545522032f38
leinefran <penny.leine@gmail.com>
2018-09-13 00:00:07 +0000

    a different approach, not correct. Consider as a draft.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if islower(str):
-        print(str)
+    for i in str:
+        if ord(str) == range(ord('a'), ord('z')+1):
+            print(str - '32')
+        else:
+            print(str)
