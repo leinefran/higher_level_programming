9f68e94fa5034ede7b082ded7e44a8ec7fe80332
leinefran <penny.leine@gmail.com>
2018-09-14 00:01:27 +0000

    Deleted: comment

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for n in range(0, 8): #n stands for numbers
+for n in range(0, 8):
     for n2 in range(1, 10):
         if n2 > n:
             print("{:d}{:d}, ".format(n, n2), end="")
