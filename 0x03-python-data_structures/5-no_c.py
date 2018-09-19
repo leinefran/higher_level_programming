2a2e3a165660bb324b2bc3270d71a26118ad6c46
leinefran <penny.leine@gmail.com>
2018-09-19 00:03:42 +0000

    Changed: the entire body of function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = list(my_string)
-    if new_string is None:
-        return (None)
-    for i in range(len(new_string)):
-        if new_string[i] == 'c' or new_string[i] == 'C':
-            del new_string[i]
-        return ''.join(new_string)
+    new_string = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
+    return (new_string)
