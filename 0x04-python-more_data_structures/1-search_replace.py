56c206e92edf8c61f74827970738ee91ccf641f1
leinefran <penny.leine@gmail.com>
2018-09-20 23:41:40 +0000

    Fixed: identation on line 4

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     def func(n):
-       return (replace if n is search else n)
+        return (replace if n is search else n)
     new_list = []
     new_list = list(map(lambda n: func(n), my_list))
     return new_list
