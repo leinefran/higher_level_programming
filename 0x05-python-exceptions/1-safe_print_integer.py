dacc4541bf57304d15811c1dc1ef1798353e0d53
leinefran <penny.leine@gmail.com>
2018-10-01 22:37:22 +0000

    Fixed: identation for return statements

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@ def safe_print_integer(value):
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
-            return True
+        return True
     except:
-        pass
         print("{} is not an interger".format(value))
-        return False
+    return False
