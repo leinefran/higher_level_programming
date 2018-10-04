482f567f8d5bf32475206245ce6eb40496687fe6
leinefran <penny.leine@gmail.com>
2018-10-04 00:35:47 +0000

    Moved: try/except inside the definition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 class Square():
-    try:
         def __init__(self, size=0):
-            self.__size = size
-    except TypeError:
-        raise TypeError("size must be an interger")
-    except ValueError:
-        raise ValueError("size must be >= 0")
+            try:
+                self.__size = size
+            except TypeError:
+                raise TypeError("size must be an interger")
+            except ValueError:
+                raise ValueError("size must be >= 0")
