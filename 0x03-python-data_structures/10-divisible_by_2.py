e969f0beec5765f7e41be219bc2f7781579fe828
leinefran <penny.leine@gmail.com>
2018-10-04 19:20:49 +0000

    Fixed: identation

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for num in range(len(my_list)):
         if num % 2 == 0:
-           new_list.append(True)
+            new_list.append(True)
         else:
             new_list.append(False)
     return new_list
