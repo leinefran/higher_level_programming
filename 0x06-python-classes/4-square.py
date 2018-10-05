e640c47bae8aea72449539b892f98b65e3dbdbef
leinefran <penny.leine@gmail.com>
2018-10-05 22:30:11 +0000

    Fixed: spelling error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
+"""
+Square Module
+"""
+
+
 class Square():
         """Created a square"""
-        def __init__(self, size=0, area=0):
+        def __init__(self, size=0):
                 """Square instantiation"""
                 if type(size) is not int:
-                        raise TypeError("size must be an interger")
+                        raise TypeError("size must be an integer")
                 if size < 0:
                         raise ValueError("size must be >= 0")
 
                 self.__size = size
 
+        @property
         def size(self):
                 return self.__size
 
+        @size.setter
         def size(self, value):
                 if type(value) is not int:
-                        raise TypeError("size must be an interger")
+                        raise TypeError("size must be an integer")
                 if value < 0:
                         raise ValueError("size must be >= 0")
 
