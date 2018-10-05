fd4dcb4c5d8a2ef2c61b29c5824446c6e0869868
leinefran <penny.leine@gmail.com>
2018-10-05 21:29:07 +0000

    Added: comments. Moved: instantiation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 class Square():
+        """created a square"""
+
         def __init__(self, size=0):
-                self.__size = size
+                """Inits Class"""
                 if type(size) is not int:
                         raise TypeError("size must be an interger")
                 if size < 0:
                         raise ValueError("size must be >= 0")
+                self.__size = size
