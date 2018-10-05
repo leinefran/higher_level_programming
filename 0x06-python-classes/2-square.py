3036c2d0324ccfaa44f17d16b0cb0e24915806de
leinefran <penny.leine@gmail.com>
2018-10-05 21:19:13 +0000

    Removed: try/except, and Used: raise exception/if statements instead

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 class Square():
         def __init__(self, size=0):
-            try:
                 self.__size = size
-            except TypeError:
-                raise TypeError("size must be an interger")
-            except ValueError:
-                raise ValueError("size must be >= 0")
+                if type(size) is not int:
+                        raise TypeError("size must be an interger")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
