bc17d83ec3e8aa3d1b2242afd7800867276d3146
leinefran <penny.leine@gmail.com>
2018-10-05 23:14:18 +0000

    Defined: Private instance attribute: position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,14 +1,48 @@
 #!/usr/bin/python3
+""""Module Square"""
+
+
 class Square():
-        def __init__(self, size=0, area=0):
-            try:
-                self.__size = size
-                self.__area = area
-            except TypeError:
-                raise TypeError("size must be an interger")
-            except ValueError:
-                raise ValueError("size must be >= 0")
-        def area(self):
-            return self.__area
-        def size(self):
-                pass
+    """Created a square"""
+    def __init__(self, size=0, area=0):
+        """Square instantiation"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+    @property
+    def position(self):
+        return self.position
+
+    @position.setter
+    def position(self, value)
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0] or value[1] is not int:
+            raise TypeError("value must be an integer")
+        if value[0] or value[1] < 0:
+            raise ValueError("value must be >= 0")
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.size):
+            print("#" * self.size)
