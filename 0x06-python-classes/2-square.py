cea62664c22f0d7ff02e1ebf690797e143079f18
leinefran <penny.leine@gmail.com>
2018-10-05 22:24:27 +0000

    Fixed: identation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,10 +5,10 @@
 class Square():
         """created a square"""
 
-        def __init__(self, size=0):
-                """Inits Class"""
-                if type(size) is not int:
-                        raise TypeError("size must be an interger")
-                if size < 0:
-                        raise ValueError("size must be >= 0")
-                self.__size = size
+    def __init__(self, size=0):
+        """Inits Class"""
+        if type(size) is not int:
+            raise TypeError("size must be an interger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
