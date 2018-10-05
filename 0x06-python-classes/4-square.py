337080dbb56855bd772c8a095b0b70aacac2e7ee
leinefran <penny.leine@gmail.com>
2018-10-05 22:44:44 +0000

    Deleted: trailing white space

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-"""
-Square Module
-"""
+"""Square Module"""
 
 
 class Square():
-        """Created a square"""
-        def __init__(self, size=0):
-                """Square instantiation"""
-                if type(size) is not int:
-                        raise TypeError("size must be an integer")
-                if size < 0:
-                        raise ValueError("size must be >= 0")
+"""Created a square"""
 
-                self.__size = size
+    def __init__(self, size=0):
+    """Square instantiation"""
 
-        @property
-        def size(self):
-                return self.__size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-        @size.setter
-        def size(self, value):
-                if type(value) is not int:
-                        raise TypeError("size must be an integer")
-                if value < 0:
-                        raise ValueError("size must be >= 0")
+    @property
+    def size(self):
+    """"Size"""
+        return self.__size
 
-                self.__size = value
+    @size.setter
+    """"DEfining size"""
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
