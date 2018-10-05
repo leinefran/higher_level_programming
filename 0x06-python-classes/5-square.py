d93d22b20c7f4f08c00a2334c5d0bb1616cfa8ed
leinefran <penny.leine@gmail.com>
2018-10-05 22:30:47 +0000

    Fixed: spelling error. Added: @property and @setter

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
 class Square():
+        """Created a square"""
         def __init__(self, size=0, area=0):
-            try:
+                """Square instantiation"""
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                if size < 0:
+                        raise ValueError("size must be >= 0")
+
                 self.__size = size
-                self.__area = area
-            except TypeError:
-                raise TypeError("size must be an interger")
-            except ValueError:
-                raise ValueError("size must be >= 0")
-        def area(self):
-            return self.__area
+
+        @property
         def size(self):
-                pass
+                return self.__size
+
+        @size.setter
+        def size(self, value):
+                if type(value) is not int:
+                        raise TypeError("size must be an integer")
+                if value < 0:
+                        raise ValueError("size must be >= 0")
+
+                self.__size = value
+
+
+        def area(self):
+            return self.__size ** 2
+
+        def my_print(self):
+                if self.__size == 0:
+                        print("")
+                for i in range(self.size):
+                        print("#" * self.size)
