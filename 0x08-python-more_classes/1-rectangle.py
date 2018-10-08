df5a44b4b9f212836d2f1d4c0cd1b2f90951e12d
leinefran <penny.leine@gmail.com>
2018-10-08 20:49:36 +0000

    Added: self.__height = value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,38 +2,39 @@
 """Empty class rectangle"""
 
 
-    class Rectangle:
-        """"Represents a class rectangle"""
+class Rectangle:
+    """"Represents a class rectangle"""
 
-        def __init__(self, width=0, height=0):
-            """Rectangle Instantiation"""
+    def __init__(self, width=0, height=0):
+        """Rectangle Instantiation"""
 
-            self.__width = width
-            self.__height = height
+        self.__width = width
+        self.__height = height
 
-        @property
-        def width(self):
-            """"Returns the width size"""
-            return self.__width
+    @property
+    def width(self):
+        """"Returns the width size"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """"Sets the width attribute with error handling"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.width = value
+    @width.setter
+    def width(self, value):
+        """"Sets the width attribute with error handling"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """"Returns the height size"""
-            return self.height
+    @property
+    def height(self):
+        """"Returns the height size"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """"Sets the height attribute with error handling"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        """"Sets the height attribute with error handling"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
