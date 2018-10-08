6f13f93d77d2334da43e726b0d8ebfdce4b273fb
leinefran <penny.leine@gmail.com>
2018-10-08 21:28:13 +0000

    Fixed: if statement inside the perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
 
     def perimeter(self):
         """"Returns the perimeter of a Rectangle"""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         p = 2*(self.__width + self.__height)
         return p
