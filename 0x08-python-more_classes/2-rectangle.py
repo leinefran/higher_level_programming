011ca17a5046608597a0b80e62a5483e34d846bf
leinefran <penny.leine@gmail.com>
2018-10-08 20:51:53 +0000

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py). Returns the area and perimeter of the rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/byn/python3
+"""Empty class rectangle"""
+
+
+class Rectangle:
+    """"Represents a class rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle Instantiation"""
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """"Returns the width size"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"Sets the width attribute with error handling"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """"Returns the height size"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """"Sets the height attribute with error handling"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """"Returns the area of the rectangle"""
+        a = self.__width * self.__height
+        return a
+
+    def perimeter(self):
+        """"Returns the perimeter of a Rectangle"""
+        p = 2*(self.__width + self.__height)
+        return p
