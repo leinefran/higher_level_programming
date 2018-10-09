97b9538c0b1461c211500c043d6941a89785ca8f
leinefran <penny.leine@gmail.com>
2018-10-09 19:19:50 +0000

    Removed: private setting for function inside de initialization and def area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,17 +8,17 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Rectangle Instantiation"""
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """"Returns the width size"""
+        """Returns the width size"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """"Sets the width attribute with error handling"""
+        """Sets the width attribute with error handling"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,12 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """"Returns the height size"""
+        """Returns the height size"""
         return self.height
 
     @height.setter
     def height(self, value):
-        """"Sets the height attribute with error handling"""
+        """Sets the height attribute with error handling"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -40,13 +40,13 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """"Returns the area of the rectangle"""
-        a = self.__width * self.__height
+        """Returns the area of the rectangle"""
+        a = self.width * self.height
         return a
 
     def perimeter(self):
-        """"Returns the perimeter of a Rectangle"""
-        if self.__width == 0 or self.__height == 0:
+        """Returns the perimeter of a Rectangle"""
+        if self.width == 0 or self.height == 0:
             return 0
-        p = 2*(self.__width + self.__height)
+        p = 2*(self.width + self.height)
         return p
