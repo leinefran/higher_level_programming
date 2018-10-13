cb9b5df2ed35de040f7fa1b04d93faabbf01ec34
leinefran <penny.leine@gmail.com>
2018-10-13 21:38:13 +0000

    Fixed: spelling error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an interger")
+        raise TypeError("b must be an integer")
     if type(a) == float or type(b) == float:
         return int(a) + int(b)
 
