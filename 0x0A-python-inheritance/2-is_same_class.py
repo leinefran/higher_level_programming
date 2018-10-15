5613902526f1b7d976c471b1c24f669969d07675
leinefran <penny.leine@gmail.com>
2018-10-15 22:53:54 +0000

    Added: if statement

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,5 +2,6 @@
 """is_same_class module"""
 def is_same_class(obj, a_class):
     """checks if a object is an instance of specified class"""
-
+    if type(obj) is object:
+        return False
     return isinstance(obj, a_class)
