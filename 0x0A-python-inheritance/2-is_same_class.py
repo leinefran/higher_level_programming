ac19674d9e4539716cf07b787ef8bbcaf4419401
leinefran <penny.leine@gmail.com>
2018-10-15 23:27:48 +0000

    Changed: body of function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """is_same_class module"""
+
+
 def is_same_class(obj, a_class):
     """checks if a object is an instance of specified class"""
-    if type(obj) is object:
-        return False
-    return isinstance(obj, a_class)
+
+    if type(obj) is a_class:
+        return True
+    return False
