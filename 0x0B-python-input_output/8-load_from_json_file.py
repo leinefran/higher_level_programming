1dd06185ae3820ce71369f6b81afb5d3c6423689
leinefran <penny.leine@gmail.com>
2018-10-17 01:06:12 +0000

    new version

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""A function that creates an Object from a “JSON file”"""
+"""A function that creates an Object from a JSON file"""
 import json
 
 
 def load_from_json_file(filename):
-    """Using loads to deserialize, with & open()"""
+    """Using load()"""
 
-    my_obj = ""
-    with open(filename, 'r') as ob:
-        while True:
-            line = ob.readline()
-            if not line:
-                break
-            my_obj += line
-
-    py_data = json.loads(my_obj)
-    return py_data
+    with open(filename, 'r') as f:
+        return json.load(f)
