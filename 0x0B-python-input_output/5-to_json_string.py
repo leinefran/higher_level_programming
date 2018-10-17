010559b51b33f8aa6201a08698300dbbb5c2b0d0
leinefran <penny.leine@gmail.com>
2018-10-17 00:14:13 +0000

    Fixed: pep8 style

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -2,6 +2,7 @@
 """A function that returns the JSON representation of an object (string)"""
 import json
 
+
 def to_json_string(my_obj):
     json_string = json.dumps(my_obj)
     return json_string
