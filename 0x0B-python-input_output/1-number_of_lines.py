be47a4ac9354229a4215e4ae8cba80afdce340c7
leinefran <penny.leine@gmail.com>
2018-10-18 18:28:23 +0000

    Removed: if statement

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,11 +7,9 @@ def number_of_lines(filename=""):
 
     num_lines = 0
 
-    with open(filename, 'rt') as f:
+    with open(filename, 'r') as f:
         """Using a for loop"""
         for line in f:
-            if line == '\n':
-                continue
             num_lines += 1
 
     return num_lines
