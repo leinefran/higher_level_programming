d00969edce00565642c1307eaa982ef51800187f
leinefran <penny.leine@gmail.com>
2018-10-18 21:37:18 +0000

    Added: If attrs is a list of strings, only attributes name contain in this list must be retrieved.

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A class Student that defines a student"""
+
+
+class Student:
+    """Represents a class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Student Instantiation"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+
+        """If attrs is a list of strings, only attributes name contain in this
+        list must be retrieved."""
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        for element in attrs:
+            if not isinstance(element, str):
+                return self.__dict__
+
+        result = {}
+        for k, v in self.__dict__.items():
+            if k in attrs:
+                result[k] = v
+        return result
