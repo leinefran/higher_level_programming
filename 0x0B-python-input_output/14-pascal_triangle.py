6793f3e7839512a4aad3dcd191b46605cb8089b7
leinefran <penny.leine@gmail.com>
2018-10-19 02:05:19 +0000

    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A function that returns a list of lists of integers
+   representing the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """A method"""
+
+    if n <= 0:
+        return [[]]
+
+    my_list = [[]]
+
+    for i in range(n):
+        nested_list = []
+        for j in range(i + 1):
+            if j == 0:
+                nested_list += [1]
+            elif j == i:
+                nested_list += [1]
+            else:
+                nested_list = my_list[i -1][j - 1] + my_list[i - 1][j]
+
+        my_list += nested_list
+
+    return my_list
