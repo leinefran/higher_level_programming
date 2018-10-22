5aa6c8f1d55babeddd64ff75094c465c41c980e9
leinefran <penny.leine@gmail.com>
2018-10-22 20:19:13 +0000

    Updated: the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,4 +32,32 @@ class Square(Rectangle):
         return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.size))
 
-    def mybrain(self):
+    def update(self, *args, **kwargs):
+        """A public method that assigns an argument to each attribute"""
+
+        idx = 0
+
+        if args and len(args) != 0:
+            for arg in args:
+                if idx == 0:
+                    self.id = arg
+                if idx == 1:
+                    self.size = arg
+                if idx == 2:
+                    self.x = arg
+                if idx == 3:
+                    self.y = arg
+
+                idx += 1
+
+        else:
+            if kwargs is not None:
+                for key, value in kwargs.items():
+                    if key == 'id':
+                        self.id = value
+                    if key == 'size':
+                        self.size = value
+                    if key == 'x':
+                        self.x = value
+                    if key == 'y':
+                        self.y = value
