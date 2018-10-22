c35d9e58af98d7bfc1ec2f7165fdee8195bb1a01
leinefran <penny.leine@gmail.com>
2018-10-22 20:02:57 +0000

    Added: a class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,21 @@ class Square(Rectangle):
         self.size = size
 
 
+    @property
+    def size(self):
+        """Returns the size of the width and height"""
+        return super().width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size attribute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Overriding the __str__ method"""
         return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
