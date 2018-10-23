00bdb1df7f2667cf9607130716016459c5e367f1
leinefran <penny.leine@gmail.com>
2018-10-23 22:43:56 +0000

    Unsing map and lambda function

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    def func(value):
-        return (value in my_list * number)
-    new_list = list(map(lambda n: func(n), my_list))
+    new_list = list(map(lambda n: n * number, my_list))
     return new_list
