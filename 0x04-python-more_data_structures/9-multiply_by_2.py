6f271036eb90d2681674ee15c1b157841959855b
leinefran <penny.leine@gmail.com>
2018-10-23 22:13:25 +0000

    Changed: body of function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return (a_dictionary.update((k, v * 2) for k, v in a_dictionary.items()))
+    new_dict = {}
+    for k, v  in a_dictionary.items():
+       new_dict[k] =  v * 2
+    return new_dict
