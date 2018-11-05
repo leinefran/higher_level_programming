ebe100341e4a4046e0902aa097b2ab7b53961ed9
leinefran <penny.leine@gmail.com>
2018-11-05 00:15:58 +0000

    working on printing the square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square():
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] or value[1] < 0:
+        elif value[0] < 0 or value[1] < 0:
             raise ValueError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -53,6 +53,7 @@ class Square():
             print("")
         else:
             for i in range(self.position[1]):
+                print()
                 if self.position[1] > 0:
-                    print(" " * self.position)
+                    print(" " * self.position[0])
                 print("#" * self.size)
