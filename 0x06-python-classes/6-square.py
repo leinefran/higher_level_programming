ae5129786337579881d72fa2a2c5e5fba5509c16
leinefran <penny.leine@gmail.com>
2018-11-05 00:07:12 +0000

    Updated: position setter and getter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,45 +4,55 @@
 
 class Square():
     """Created a square"""
-    def __init__(self, size=0, area=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Square instantiation"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """"Size getter"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Size setter"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
+
     @property
     def position(self):
-        return self.position
+        """Position getter"""
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if type(position) is not tuple:
+        """Position setter"""
+        if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(position) is not 2:
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0] or value[1] is not int:
-            raise TypeError("value must be an integer")
-        if value[0] or value[1] < 0:
-            raise ValueError("value must be >= 0")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] or value[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
 
     def my_print(self):
         if self.__size == 0:
             print("")
-        for i in range(self.size):
-            print("#" * self.size)
+        else:
+            for i in range(self.position[1]):
+                if self.position[1] > 0:
+                    print(" " * self.position)
+                print("#" * self.size)
