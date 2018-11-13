9c36f37e68d4a9066dffd0db1f11cbb6daaaa8ea
leinefran <penny.leine@gmail.com>
2018-11-13 17:19:35 +0000

    Deleted: underscores on self.width and self.height in the init method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         """Rectangle Instantiation"""
         type(self).number_of_instances += 1
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
