71538a5159d1640dcb35275125ae5aec197536df
leinefran <penny.leine@gmail.com>
2018-11-13 01:52:44 +0000

    Added: a if statement to the str method

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -63,7 +63,8 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rect += str(self.print_symbol)
-            rect += "\n"
+            if i != self.__height - 1:
+                rect += "\n"
         return rect
 
     def __repr__(self):
