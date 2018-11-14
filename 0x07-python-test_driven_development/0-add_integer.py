a37e53691eb882b5b87b2df1d0e3fe3224264135
leinefran <penny.leine@gmail.com>
2018-11-14 22:46:33 +0000

    Fixed: documentation format

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,9 +3,10 @@
 
 
 def add_integer(a, b=98):
-    """Adds 2 integers. If the 2nd argv is not provided,
+    '''
+    Adds 2 integers. If the 2nd argv is not provided,
     it is set to 98 by default
-    """
+    '''
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
