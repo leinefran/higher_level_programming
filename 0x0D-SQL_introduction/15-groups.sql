c9402cdd651b6e21061fcf46377977845089dcd8
leinefran <penny.leine@gmail.com>
2018-12-04 01:52:33 +0000

    Changed ORDER BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- a script that lists the number of records with the same score in the table second_table.
-SELECT score, COUNT(*) As number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) As number FROM second_table GROUP BY score ORDER BY number DESC;
