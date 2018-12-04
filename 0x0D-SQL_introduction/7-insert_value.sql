9e81d5fc4a011708a62ae281237615c00a73ed0c
leinefran <penny.leine@gmail.com>
2018-12-04 00:02:12 +0000

    a script that inserts a new row in the table first_table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- A script that inserts a new row in the table first_table.
+-- New row: id = 89, name = Holberton School.
+-- The database name will be passed as an argument of the mysql command.
+INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
