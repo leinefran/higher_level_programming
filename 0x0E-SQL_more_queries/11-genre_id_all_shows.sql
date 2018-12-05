5ae138ec32fb9c66053f4d8597f6fed4de1de40e
leinefran <penny.leine@gmail.com>
2018-12-05 04:51:01 +0000

    Changed JOIN Statement

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,10 +2,9 @@
 -- that have at least one genre linked.
 
 SELECT
-	tv_shows.title, tv_show_genres.genre_id
+       tv_shows.title, tv_show_genres.genre_id
 FROM
 	tv_shows
-	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-
+	FULL JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY
       tv_shows.title ASC, tv_show_genres.genre_id ASC;
