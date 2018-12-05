91082a76e597c962b0e954d9665ef8627fe2af2b
leinefran <penny.leine@gmail.com>
2018-12-05 20:39:28 +0000

    Fixed id values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,4 @@
 -- a script that creates a table called second_table in the current database in your MySQL server.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10),
+       (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
