3766a9fa9fe70c5ca5cd64eaf40053e4da2dfa2f
leinefran <penny.leine@gmail.com>
2018-12-05 00:07:03 +0000

    a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,10 @@
 -- that have at least one genre linked.
 
 SELECT
-	title, genre_id
+	tv_shows.title, tv_show_genres.genre_id
 FROM
-	tv_shows, tv_show_genres
+	tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+
 ORDER BY
       tv_shows.title ASC, tv_show_genres.genre_id ASC;
