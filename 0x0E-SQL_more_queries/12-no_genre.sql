1ad3e5e2f89c11d69d734869339b1ce5bd1a31b4
leinefran <penny.leine@gmail.com>
2018-12-05 04:57:07 +0000

    Added: WHERE

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,5 +6,7 @@ SELECT
 FROM
 	tv_shows
 	LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE
+	tv_show_genres.genre_id IS NULL
 ORDER BY
       tv_shows.title ASC, tv_show_genres.genre_id ASC;
