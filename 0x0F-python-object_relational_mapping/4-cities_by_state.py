209d39378bbc2ccab8d695707d88f544ef9974e2
leinefran <penny.leine@gmail.com>
2019-01-03 02:47:53 +0000

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a script that lists all cities from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+"""Connect to the database"""
+db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+"""Create a cursor object"""
+cur = db.cursor()
+
+"""Grab all cities in ascending order"""
+cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id")
+query_rows = cur.fetchall()
+for row in query_rows:
+        print ('{}'.format(row))
