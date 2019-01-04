cf0dbd9f0f3c03e86373d05d1f085475c340e1ed
leinefran <penny.leine@gmail.com>
2019-01-04 23:46:10 +0000

    Fixed: pep8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     """Grab all cities in ascending order"""
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities,
-    states WHERE cities.state_id=states.id ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities, " +
+                "states WHERE cities.state_id=states.id ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
