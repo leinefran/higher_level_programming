2451ff1459ab296679c4cb3287e75d5cc557d84e
leinefran <penny.leine@gmail.com>
2019-01-04 23:19:54 +0000

    Fixed: pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print ('{}'.format(row))
+        print('{}'.format(row))
 
     # Close all cursors
     cur.close()
