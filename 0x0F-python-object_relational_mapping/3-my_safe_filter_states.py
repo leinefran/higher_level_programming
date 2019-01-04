74ea7cb2a69ae5c89135026f1c02eb331d501e17
leinefran <penny.leine@gmail.com>
2019-01-04 23:27:00 +0000

    Fixed: pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     """Create a cursor object"""
     cur = db.cursor()
 
-    input_name = sys.argv[4];
+    input_name = sys.argv[4]
 
     """Grab all states in ascending order"""
     cur.execute("SELECT * FROM states WHERE name = %s;", (input_name,))
     query_rows = cur.fetchall()
     for row in query_rows:
-        print ('{}'.format(row))
+        print('{}'.format(row))
 
     # Close all cursors
     cur.close()
