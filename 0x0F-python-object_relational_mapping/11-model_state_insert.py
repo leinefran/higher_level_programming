61541638660bf7aac07483fe18dceaed9a0ef83c
leinefran <penny.leine@gmail.com>
2019-01-05 01:15:49 +0000

    Fixed: body of script

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     new_state = State(name="Louisiana”)
     session.add(new_state)
     session.commit()
-    print(new_state.id)
-    session.close()
+    louisiana = session.query(State).filter_by(name="Louisiana").first()
+    print(louisiana.id)
+    session.commit()
