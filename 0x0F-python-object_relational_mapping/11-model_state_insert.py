cc9736b22fc3148efb2b785647287464102e7360
leinefran <penny.leine@gmail.com>
2019-01-05 01:19:07 +0000

    Fixed: string literal

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana”)
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
-    louisiana = session.query(State).filter_by(name="Louisiana").first()
-    print(louisiana.id)
+    Louisiana = session.query(State).filter_by(name='Louisiana').first()
+    print(Louisiana.id)
     session.commit()
