d88492644723546c6fc64e1e3c3d0025cfdf7bf7
leinefran <penny.leine@gmail.com>
2019-01-21 21:13:46 +0000

    Added: comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,11 @@ import urllib.request
 
 if __name__ == "__main__":
 
+    # fetch internet resources with the Python package urllib:
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         t = type(html)
+        # decode urllib body response:
         output = html.decode(response.headers.get_content_charset())
 
         print("Body response:")
