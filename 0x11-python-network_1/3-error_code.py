ccb95ef417cc2871a25221b0be6d4199951f9f34
leinefran <penny.leine@gmail.com>
2019-01-22 04:45:13 +0000

    Added: import packages

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,12 @@
 '''a Python script that takes in a URL, sends a request to the URL
 & displays the body of the response (decoded in utf-8)'''
 
-if __name__ == '__main__':
-    url = sys.argv[1]
+import urllib
+import sys
 
+if __name__ == "__main__":
     try:
-        req = urllib.request.Request(url)
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
