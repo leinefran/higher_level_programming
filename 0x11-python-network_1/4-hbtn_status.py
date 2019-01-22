fd56ebe983d2f10bd9d5f7637ce53d00c744db47
leinefran <penny.leine@gmail.com>
2019-01-22 02:22:04 +0000

    a Python script that fetches https://intranet.hbtn.io/status using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''A Python script that fetches a URL'''
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = type(r)
+
+    print("Body response:")
+    print("\t- type: {}".format(t))
+    print("\t- content: {}".format(r))
