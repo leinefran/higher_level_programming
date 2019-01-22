351783a9779965ecd0000dac4f42e980ba7b4b18
leinefran <penny.leine@gmail.com>
2019-01-22 03:28:27 +0000

    Changed: if and else statement

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if r.status_code == request.codes.ok:
+    if r.status_code == requests.codes.ok:
         print(r.text)
     else:
-        print("Error code: {}". format(r.status.code))
+        print("Error code: {}". format(r.status_code))
