583fc54846cb55e13b33d1eb7e9bbd3bea30a10f
leinefran <penny.leine@gmail.com>
2019-01-22 03:12:46 +0000

    Changed: name of variabele values to email, and Switched get() to post()

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
-    r = requests.get(url, params=values)
+    email = {'email': sys.argv[2]}
+    r = requests.post(url, email)
 
     print(r.text)
