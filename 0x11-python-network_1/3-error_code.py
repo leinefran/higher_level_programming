3f42f721485b54dabfd832e6432efbe7b4f06cfc
leinefran <penny.leine@gmail.com>
2019-01-23 00:54:13 +0000

    Fixed: import package name and variable name in with statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,14 +2,14 @@
 '''a Python script that takes in a URL, sends a request to the URL
 & displays the body of the response (decoded in utf-8)'''
 
-import urllib
+import urllib.request
 import sys
 
 if __name__ == "__main__":
 
 
     try:
-        with urllib.request.urlopen(argv[1]) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
