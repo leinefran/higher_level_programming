6236b9485bc517f0e4b0ac0baffdc9f226050bcf
leinefran <penny.leine@gmail.com>
2019-01-23 02:22:17 +0000

    Added: body of script

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 '''a Python script that takes your Github credentials
-& uses the Githib API to display your id'''
+& uses the Github API to display your id'''
 
 import requests
 import sys
 if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(username, password))
+
+    try:
+        print(r.json().pop('id'))
+    except:
+        print("None")
