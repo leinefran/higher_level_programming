ee8b7a20a91162d425eafbd105a1b67d73991bd2
leinefran <penny.leine@gmail.com>
2019-01-23 00:55:09 +0000

    Fixed: pep8

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,8 +6,6 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-
-
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
