0f552a65124d55471829bef3099cfdbebf2bdf6a
leinefran <penny.leine@gmail.com>
2019-02-10 00:42:50 +0000

    Changed: if condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
 
     new_tuple = ()
 
-    if not sentence or if sentence == "":
+    if not sentence or if len(sentence) == 0:
         new_tuple = 'None'
     else:
         new_tuple = len(sentence), sentence[0]
