ef11500852bbfb64282263c9855c42d858464a3e
leinefran <penny.leine@gmail.com>
2019-02-10 01:05:03 +0000

    Deleted: elif statement, and Changed: if statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,10 +3,8 @@ def multiple_returns(sentence):
 
     new_tuple = ()
 
-    if not sentence:
-        new_tuple = 'None'
-    elif len(sentence) == 0:
-        new_tuple = (0, sentence[0])
+    if len(sentence) == 0:
+        new_tuple = len(sentence), 'None'
     else:
         new_tuple = len(sentence), sentence[0]
 
