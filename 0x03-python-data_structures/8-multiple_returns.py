394d1740d4f588187bad5710f2336b122b0a8be1
leinefran <penny.leine@gmail.com>
2019-02-10 00:45:10 +0000

    Added: an extra condition to the if statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,10 @@ def multiple_returns(sentence):
 
     new_tuple = ()
 
-    if not sentence or if len(sentence) == 0:
+    if not sentence:
         new_tuple = 'None'
+    else if len(sentence) == 0:
+        len(sentence), 'None'
     else:
         new_tuple = len(sentence), sentence[0]
 
