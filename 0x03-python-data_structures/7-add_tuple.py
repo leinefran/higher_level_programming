d685aff1a58e588faa6644191342c38e17d42e98
leinefran <penny.leine@gmail.com>
2019-02-10 00:18:50 +0000

    Added: extra if statements to handle all edge cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > 1 and len(tuple_b) > 1:
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_b[1] + tuple_b[1]
+    new_tuple = ()
 
-        return (a, b)
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        new_tuple = (0, 0)
 
-    else:
-        tuple_a = tuple_a(0,0)
-        tuple_b = tuple_b(0,0)
+    if len(tuple_a) == 0 and len(tuple_b) == 1:
+        new_tuple = 0, tuple_b[0]
 
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_b[1] + tuple_b[1]
+    if len(tuple_a) == 1 and len(tuple_b) == 0:
+        new_tuple = tuple_a[0], 0
 
-        return (a, b)
+    if len(tuple_a) == 0 and len(tuple_b) >= 2:
+        new_tuple = tuple_b[0], tuple_b[1]
+
+    if len(tuple_a) >= 2 and len(tuple_b) == 0:
+        new_tuple = tuple_a[0], tuple_a[1]
+
+    if len(tuple_a) >= 2 and len(tuple_b) == 1:
+        new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1]
+
+    if len(tuple_a) == 1 and len(tuple_b) >= 2:
+        new_tuple = tuple_a[0] + tuple_b[0], tuple_b[1]
+
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        new_tuple = tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_b[1]
+
+    return (new_tuple)
