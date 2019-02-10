298f6c99d3adf565cb33cb7bd35eb6bef323906d
leinefran <penny.leine@gmail.com>
2019-02-10 00:34:37 +0000

    Rewrote: entire body of the function

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+
+    new_tuple = ()
+
     if not sentence:
-        sentence[0] = None
-    return [len(sentence), sentence[0]]
+        new_tuple = 'None'
+    else:
+        new_tuple = len(sentence), sentence[0]
+
+    return new_tuple
