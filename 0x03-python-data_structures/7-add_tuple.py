8db4d423112dbd4bfec0c98e7e008883907ef793
leinefran <penny.leine@gmail.com>
2019-02-10 00:22:11 +0000

    Added: another if statement for when both tuple lengths are 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,6 +11,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1 and len(tuple_b) == 0:
         new_tuple = tuple_a[0], 0
 
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        new_tuple = tuple_a[0], tuple_b[0]
+
     if len(tuple_a) == 0 and len(tuple_b) >= 2:
         new_tuple = tuple_b[0], tuple_b[1]
 
