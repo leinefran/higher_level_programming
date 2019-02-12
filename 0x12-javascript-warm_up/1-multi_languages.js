412cbe044db0cf78f2a2f2ef3e3136a4833d56b0
leinefran <penny.leine@gmail.com>
2019-02-12 21:48:14 +0000

    a script that prints 3 lines of text

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* a scrip that prints:
+   "C is fun"
+   "Python is cool"
+   "Javascript is amazing" */
+
+// use console.log() to print output.
+
+let first = 'C is fun';
+let second = 'Python is cool';
+let third = 'Javascript is amazing';
+
+console.log(first);
+console.log(second);
+console.log(third);
