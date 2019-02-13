c32c819b8007af0d3257fd431038622bf515a7d2
leinefran <penny.leine@gmail.com>
2019-02-13 04:02:58 +0000

    Added: help function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,14 @@
 // a script that searches the 2nd biggest integer in the list of arguments.
 // use console.log() to print output.
 
+function sortNumber (a, b) {
+  return a - b;
+}
 let myArray = process.argv;
 
 if (myArray.length < 3) {
   console.log('0');
 } else {
-  myArray.sort();
+  myArray.sort(sortNumber);
   console.log(myArray[myArray.length - 2]);
 }
