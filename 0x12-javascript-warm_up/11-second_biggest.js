b210a83bf4be4241b125ad54ea909366175e2d79
leinefran <penny.leine@gmail.com>
2019-02-13 03:49:02 +0000

    a script that searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that searches the 2nd biggest integer in the list of arguments.
+// use console.log() to print output.
+
+let myArray = process.argv;
+
+if (myArray.length < 3) {
+  console.log('0');
+} else {
+  myArray.sort();
+  console.log(myArray[myArray.length - 2]);
+}
