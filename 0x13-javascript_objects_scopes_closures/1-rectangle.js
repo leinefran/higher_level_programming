7c0f2e9d9f76571080ec997e378a0ef4efc3fa51
leinefran <penny.leine@gmail.com>
2019-02-18 21:57:04 +0000

    First Draft: a class Rectangle that defines a rectangle, and that takes two arguments

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// an empty class Retangle that defines a rectangle;
+// and takes two arguments: width and height;
+
+class Rectangle {
+  constructor (height, width) {
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
