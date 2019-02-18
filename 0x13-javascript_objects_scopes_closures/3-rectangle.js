7e1292508565fdd153ebfb3f812d8cc20c56f325
leinefran <penny.leine@gmail.com>
2019-02-18 23:18:29 +0000

    a class rectangle that defines a rectangle, and have an instance method called print() that prints the rectangle using the character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// create an instance method called print();
+// that prints a rectangle using the character X;
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    console.log('X'.repeat(this.width).concat('\n').repeat(this.height)
+	       .slice(0, -1));
+  }
+};
