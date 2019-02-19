20f0502cc6767d0ef59ec8f799935ade45d37603
leinefran <penny.leine@gmail.com>
2019-02-19 00:31:59 +0000

    a class Square that defines a square and inherits from Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+// a class square that inherits from rectangle;
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    console.log('X'.repeat(this.width).concat('\n').repeat(this.height).slice(0, -1));
+  }
+  rotate () {
+    // swap!
+    [this.height, this.width] = [this.width, this.height];
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor(size) {
+    super(size, size)
+  }
+};
