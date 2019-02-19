26b86c78259662029123ae89522f26d479723100
leinefran <penny.leine@gmail.com>
2019-02-19 03:15:39 +0000

    a function that prints the number of arguments already printed and the new argument value

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a function that prints the number of arguments already printed and the argument value;
+
+let countPrint = 0;
+exports.logMe = function (item) {
+  if (item) {
+    console.log(countPrint + ': ' + item);
+    countPrint +=1;
+  }
+}
