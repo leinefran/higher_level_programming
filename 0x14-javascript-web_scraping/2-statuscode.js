11064212a2b2544905db7b3815d7acc4e06f41da
leinefran <penny.leine@gmail.com>
2019-02-19 21:43:42 +0000

    a script that display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* a script that display the status code of a GET request;
+The first argument is the URL to request (GET)
+The status code must be printed like this: code: <status code>
+You must use the module request */
+
+const request = require('request');
+
+request.get(process.argv[2], function(err, res) {
+  if (err) {
+    console.log('error:', error); // Print the error if one occurred
+  } else {
+    console.log('code :', res.statusCode); // Print the res status code
+  }
+});
