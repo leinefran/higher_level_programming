1ccf8d3e0607c21f6c29277d8cc466b3d49f3b18
leinefran <penny.leine@gmail.com>
2019-02-21 23:22:11 +0000

    a Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+/* a Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000):
+ * You must use document.querySelector to select the HTML tag
+ * You can’t use the jQuery API */
+
+const header = document.querySelector('header').style.color = "#FF0000";
