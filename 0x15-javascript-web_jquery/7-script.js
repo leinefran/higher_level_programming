7a97e2ad5974ecc9fe7d0c2bc409255248f1132f
leinefran <penny.leine@gmail.com>
2019-02-22 05:58:04 +0000

    a Javascript script that fetches and replaces the name of this URL: https://swapi.co/api/people/5/?format=json

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+/* a Javascript script that fetches and replaces the name of this URL: https://swapi.co/api/people/5/?format=json
+
+The name must be displayed in the HTML tag DIV#character
+You can’t use document.querySelector to select the HTML tag
+You must use the jQuery API */
+
+$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+  $('#character').replaceWith(data.name);
+});
