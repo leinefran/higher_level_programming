4efdcd8c9e3095854d3108e3f9ae95b749a3d75c
leinefran <penny.leine@gmail.com>
2019-02-22 01:18:56 +0000

    Fixed: style semistandard

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -2,4 +2,4 @@
  * You must use document.querySelector to select the HTML tag
  * You can’t use the jQuery API */
 
-const header = document.querySelector('header').style.color = "#FF0000";
+document.querySelector('header').style.color = '#FF0000';
