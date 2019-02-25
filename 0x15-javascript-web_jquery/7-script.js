e3aa9b31dfcd23afd70226b456eb49e4d88e10af
leinefran <penny.leine@gmail.com>
2019-02-25 23:16:53 +0000

    Replaced: replaceWith() to text()

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -5,5 +5,5 @@ You can’t use document.querySelector to select the HTML tag
 You must use the jQuery API */
 
 $.get('https://swapi.co/api/people/5/?format=json', function (data) {
-  $('#character').replaceWith(data.name);
+  $('DIV#character').text(data.name);
 });
